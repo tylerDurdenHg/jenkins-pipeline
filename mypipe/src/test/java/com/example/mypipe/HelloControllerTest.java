04b31bd2bac992a55f3f8bf8ee7728f1bfdbbf0f
Tyler Durdenhg <you@example.com>
2020-06-13 12:11:15 +0300

    @Test

---
+++ mypipe/src/test/java/com/example/mypipe/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.mypipe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloControllerTest {
+	
+	@Test
+	public void hello() {
+		assertEquals(1, 11);
+	}
+
+}
