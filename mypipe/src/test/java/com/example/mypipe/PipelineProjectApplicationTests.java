7aa10efe4bb389b4f1f4c9cd8d8a085ff0212c47
Tyler Durdenhg <you@example.com>
2020-06-13 11:25:27 +0300

    hello project

---
+++ mypipe/src/test/java/com/example/mypipe/PipelineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
